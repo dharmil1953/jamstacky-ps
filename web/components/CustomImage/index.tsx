7a60bf718c8dbfc5ebdae2a4e376b030a9868b77
bhargav <dharmilkotak00@gmail.com>
2024-08-31 12:51:51 +0530

    Image Component

--- web/components/CustomImage/index.tsx
+++ web/components/CustomImage/index.tsx
@@ -1,5 +1,7 @@
-import { Image } from "../Image";
+// import { Image } from "../Image";
 import { ImageProps } from "next/image";
+import Image from 'next/image'
+
 import { CustomImageType } from "@/lib/sanity/types";
 import dynamic from "next/dynamic";
 // const Link = dynamic(() => import("../Link"));

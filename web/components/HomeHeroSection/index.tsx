f2beb088a7b8efe9880042f4c0d0c75db4f26638
bhargav <dharmilkotak00@gmail.com>
2024-09-04 14:41:47 +0530

    trying

--- web/components/HomeHeroSection/index.tsx
+++ web/components/HomeHeroSection/index.tsx
@@ -16,7 +16,7 @@ const Hero: React.FC<HomeheroSectionType> = (sectionProps) => {
     title_image,
     section_theme,
   } = sectionProps || {};
-  
+
   const [showAnimation, setShowAnimation] = useState(false);
   useEffect(() => {
     const timeout = setTimeout(() => {
@@ -37,6 +37,7 @@ const Hero: React.FC<HomeheroSectionType> = (sectionProps) => {
     >
       <Head>
         <link rel="preload" href={hero_image?.image?.asset?.url} as="image" />
+        <link rel="preload" href={bottom_description} />
       </Head>
 
       <div className="container">
@@ -106,7 +107,7 @@ const Hero: React.FC<HomeheroSectionType> = (sectionProps) => {
             )}
           </div>
         </div>
-        <div className="max-w-[80%] esm:max-w-[70%] lg:max-w-[64%] text-center mt-0 em:mt-12 mb-0 mx-auto pb-12 pt-0 em:pt-12 sm:pt-16">
+        <div className="max-w-4/5 esm:max-w-[70%] em:max-w-4/5 lg:max-w-[64%] relative z-0 text-center mt-0 em:mt-12 mb-0 my-0 mx-auto pb-12 pt-0 em:pt-12 sm:pt-16">
           <p className="text-sm em:text-base md:text-lg lg:text-xl leading-relaxed font-normal text-dark-blue">
             {bottom_description}
           </p>

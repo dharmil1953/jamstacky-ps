b6001d1f889ddf78f6bbb0548478fd905d9a2e39
bhargav <dharmilkotak00@gmail.com>
2024-09-04 13:27:52 +0530

    Revert "LCP Part Remove"
    This reverts commit 4aa5d0cd11c4daa09e4d2fa519de1a3753ad8c29.

--- web/components/HomeHeroSection/index.tsx
+++ web/components/HomeHeroSection/index.tsx
@@ -16,7 +16,6 @@ const Hero: React.FC<HomeheroSectionType> = (sectionProps) => {
     title_image,
     section_theme,
   } = sectionProps || {};
-  
   const [showAnimation, setShowAnimation] = useState(false);
   useEffect(() => {
     const timeout = setTimeout(() => {
@@ -26,7 +25,7 @@ const Hero: React.FC<HomeheroSectionType> = (sectionProps) => {
       clearTimeout(timeout);
     };
   }, []);
-  // console.log()
+  // console.log(hero_image)
   return (
     <div
       className={clsx(
@@ -36,7 +35,7 @@ const Hero: React.FC<HomeheroSectionType> = (sectionProps) => {
       )}
     >
       <Head>
-        <link rel="preload" href={hero_image?.image?.asset?.url} as="image" />
+        <link rel="preload" href={hero_image} as="image" />
       </Head>
 
       <div className="container">
@@ -106,11 +105,11 @@ const Hero: React.FC<HomeheroSectionType> = (sectionProps) => {
             )}
           </div>
         </div>
-        {/* <div className="max-w-[80%] esm:max-w-[70%] lg:max-w-[64%] text-center mt-0 em:mt-12 mb-0 mx-auto pb-12 pt-0 em:pt-12 sm:pt-16">
+        <div className="max-w-[80%] esm:max-w-[70%] lg:max-w-[64%] text-center mt-0 em:mt-12 mb-0 mx-auto pb-12 pt-0 em:pt-12 sm:pt-16">
           <p className="text-sm em:text-base md:text-lg lg:text-xl leading-relaxed font-normal text-dark-blue">
             {bottom_description}
           </p>
-        </div> */}
+        </div>
       </div>
     </div>
   );
